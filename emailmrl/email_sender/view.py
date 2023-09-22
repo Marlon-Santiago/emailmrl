1a9e3bdb2f688f3039f1dd0b602c4c037a50dbd6
Fawke-s <83735637+Marlon-Santiago@users.noreply.github.com>
2023-09-21 23:06:11 -0300

    .

--- emailmrl/email_sender/view.py
+++ emailmrl/email_sender/view.py
@@ -1,35 +1,63 @@
 import PySimpleGUI as sg
 from PySimpleGUI import WIN_CLOSED
+from emailmrl.utils import inner_element_space
 
 listas = ['Administradores', 'Alunos']
 
 def get_layouts():
-    layout = [
+    inner_element_space(500),
+    
+    frame_campaing = [
+        
         [
             sg.Text('Selecione o seu codigo'),
-            sg.In(),
-            sg.FilesBrowse('Selecione', file_types = (('Codigo Python', '*.py'),)),
+            sg.In(key = '-Code-', size = (30, 1)),
+            sg.FilesBrowse('Selecione', file_types = (('Codigo Python', '*.py'),), size = (15, 1)),
         ],
         [
             sg.Text('Seleciona a lista de destinatario'),
-            sg.Combo(listas, default_value = listas[1]),
-        ],
-        [
-            sg.Text('Insira o Titulo: '),
-            sg.In(key = '-Title-'),
-        ],
-        [
-            sg.Text('Insira o conteudo do email'),
-            sg.MLine(key = '-Content-'),
-        ],
-        [
-            sg.Button('Enviar', key = '-Send-'),
-            sg.Button('Gerenciar listas', key = '-ListEditor-')
+            sg.Combo(listas, default_value = listas[1], key = '-Lists-',),
         ]
+    ]
+
+    frame_email = [
+        inner_element_space(500),
+        [
+            sg.Text('Insira o Titulo:', font = ('Helvetica 15')),
+        ],
+        [
+            sg.In(key = '-Title-', size = (62, 1)),
+        ],
+        [
+            sg.Text('Insira o conteudo', font = ('Helvetica 15')),
+        ],
+        [
+            sg.MLine(key = '-Content-', size = (60, 10)),
+        ],
+        inner_element_space(500)
+    ]
+    
+
+
+
+    layout = [
+        [
+            sg.Frame('Configuração da campanha', frame_campaing, element_justification = 'c'),
+        ],
+        [
+            sg.Frame('Configuração de email', frame_email, element_justification = 'c'),
+        ],
+        [
+            sg.Button('Enviar', key = '-Send-', size = (15, 1), pad = (10, (10, 0))),
+            sg.Button('Gerenciar listas', key = '-ListEditor-', size = (15, 1), pad = (10, (10,0))),
+        ],
+        inner_element_space(500),
+        
     ]
         
         
     return layout
 
 def get_windows():
-    return sg.Window('Teste de Janela', get_layouts())
+    sg.theme('DarkBlue')
+    return sg.Window('Enviador de E-mail', get_layouts())
--- emailmrl/list_editor/view.py
+++ emailmrl/list_editor/view.py
@@ -1,10 +1,12 @@
 import PySimpleGUI as sg
+from emailmrl.utils import inner_element_space
 
 lista = ['Administrador','Aluno']
 
 def get_layout():
     
     frame_list = [
+        inner_element_space(600),
         [
             sg.Text('Selecione a lista'),
             sg.Combo(lista, default_value = lista[1], key ='-List-'),
@@ -12,21 +14,58 @@ def get_layout():
         [
             sg.Text('Criar uma lista'),
             sg.In(key = '-ListName-'),
-            sg.Button('Criar a lista', key = '-Create-', size = (10, 1)),
+            sg.Button('Criar a lista', key = '-Create-', size = (15, 1), pad = (0, (7, 7))),
         ],
         [
-            sg.Button('Deletar uma lista', key = '-Delete-', size = (15, 1)),
-            sg.Button('Mostrar contatos', key = '-Delete-', size = (15, 1)),
+            sg.Button('Deletar uma lista', key = '-Delete-', size = (15, 1), pad = (5, (7, 7))),
+            sg.Button('Mostrar contatos', key = '-Delete-', size = (15, 1), pad = (5, (7, 7))),
+        ],
+    ]
+    
+    frame_import = [
+        inner_element_space(600),
+        [
+            sg.Text('Selecione um arquivo (CSV):', tooltip = 'Cabeçalhos: name e email'),
+            sg.In(key = '-CSV-'),
+            sg.FileBrowse('Selecionar', file_types = (('CSV', '*.csv'),), tooltip = 'cabeçalhoe: name e email',),
+        ],
+        [
+            sg.Button('Importar contatos', key = '-Import-', size = (15, 1), pad = (0, (7, 7)))
+        ]
+    ]
+    
+    frame_add = [
+        inner_element_space(600),
+        [
+            sg.Text('Insira o nome:'),
+            sg.In(key = '-Name-'),
+        ],
+        [
+            sg.Text('insira o email:'),
+            sg.In(key = '-Email-'),
+        ],
+        [
+            sg.Button('Adicionar contato', key = '-Add-', size = (15, 1), pad = (5, (7, 7)))
         ],
     ]
     
     layouts = [
         [
-            sg.Frame('Configuirações da lista', frame_list)
+            sg.Frame('Configurações da lista', frame_list, element_justification = 'c')
+        ],
+        [
+            sg.Frame('Importa contatos', frame_import, element_justification = 'c')
+        ],
+        [
+            sg.Frame('Adiçionar contato', frame_add, element_justification = 'c')
+        ],
+        [
+            sg.Button('Voltar', key = '-Back-', size = (15, 1), pad = (5, (7, 7)))
         ]
     ]
     
     return layouts
 
 def get_window():
-    return sg.Window('Editor de lista', get_layout())
+    sg.theme('DarkBlue')
+    return sg.Window('Editor de lista', get_layout(), element_justification = 'c')
